5d6ddc4f16e9a88a187072685ad11b77630e4f2f
Judi-fr <juanitoalcachofa55@hotmail.com>
2025-08-21 00:11:19 -0300

    update, migrations, se agrego tabla pene y personita en model

--- miweb/app/models.py
+++ miweb/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Personita(models.Model):
+    name = models.CharField(max_length=200)
+
+class Pene(models.Model):
+    largo = models.IntegerField()
+    team = models.CharField(max_length=8) #sangre o carne
+    color = models.CharField(max_length=10) #de piel
+    propietario = models.ForeignKey(
+        Personita,
+        on_delete=models.CASCADE,
+        related_name="penes")
+
